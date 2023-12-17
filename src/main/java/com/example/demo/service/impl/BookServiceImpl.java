b22fb616c9cf9fb23d7e8de693c12558de6a09d5
Lav <laverson.espindola@gmail.com>
2023-12-17 10:29:22 -0300

    Pipeline test

--- src/main/java/com/example/demo/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BookServiceImpl.java
@@ -12,7 +12,7 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<BookResponse> findAll() {
         List<BookResponse> list = new ArrayList<>();
-        list.add(new BookResponse(1L,"O SENHOR DOS ANEIS - A Sociedade do Anel",
+        list.add(new BookResponse(1L, "O SENHOR DOS ANEIS - A Sociedade do Anel",
                 "Em uma terra fantástica e única, " +
                         "um hobbit recebe de presente de seu tio um anel mágico " +
                         "e maligno que precisa ser destruído antes que caia nas mãos do mal. " +
@@ -22,6 +22,11 @@ public class BookServiceImpl implements BookService {
                         "ele aos poucos pode contar com outros hobbits, " +
                         "um elfo, um anão, dois humanos e um mago, " +
                         "totalizando nove seres que formam a Sociedade do Anel."));
+        list.add(new BookResponse(1L, "DUNA",
+                "Here is the novel that will be forever considered a triumph of the imagination."+
+                 "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, who would become the" +
+                 "mysterious man known as Muad'Dib.He would avenge the traitorous plot against his noble family--and would bring " +
+                 "to fruition humankind's most ancient and unattainable dream."));
         return list;
     }
 }
